9cf949880adb3c8bcd72997038a9cd183b46e039
Jenny Ramirez <jennyramirez@Jennys-MacBook-Air.local>
2021-01-15 01:33:33 -0500

    updated some features to the user.js route file and added the profile route to the user.js route file.

--- cafe-your-way-backend/routes/users.js
+++ cafe-your-way-backend/routes/users.js
@@ -9,7 +9,7 @@ var orders = require('../models/orders');
 
 
 
-// POST SIGNUP users (users sign up and create an account if an account does not exist and returns JWT token as cookie) - (Accessible by All Users)
+// POST SIGNUP users (users sign up and create an account if an account does not exist and returns JWT token as cookie).  Password is hashed/encrypted - (Accessible by All Users)
 router.post('/signup', function (req, res) {
   models.users.findOrCreate({
     where: { UserName: req.body.UserName },
@@ -29,15 +29,19 @@ router.post('/signup', function (req, res) {
   })
     .spread(function (result, created) {
       if (created) {
-        res.json({ message: 'User successfully created' });
+        // Status 200: OK status/success
+        res.json({ message: 'User successfully created', status: 200 });
       } else {
-        res.json({ message: 'This user already exists' });
+        // Status 400:  bad request/client error
+        res.json({ message: 'This user already exists', status: 400 });
       }
     });
 });
 
 
-// POST LOGIN (users login and JWT is returned as cookie.  User is authenticated with a JWT token. Also, checks if the requesting user is marked asn "Admin" in the database. (Accessible by All Users)
+
+
+// POST LOGIN (users login and JWT is returned as cookie. The authorization service used is  ".signUser ()" method".  User is authenticated with a JWT token. Also, checks if the requesting user is marked as an "Admin" in the database. (Accessible by All Users)
 router.post('/login', function (req, res) {
   models.users.findOne({
     where: {
@@ -50,19 +54,23 @@ router.post('/login', function (req, res) {
         if (passwordMatch) {
           let token = authService.signUser(user);
           res.cookie('jwt', token, { httpOnly: true });
-          res.json({ message: 'User successfully logged in' });
+          // Status 200: OK status/success
+          res.json({ message: 'User successfully logged in', status: 200, token });
         } else {
-          res.json({ message: 'wrong password' });
+          // Status 400:  bad request/client error
+          res.json({ message: 'wrong password', status: 400 });
         }
       } else {
-        res.json({ message: 'wrong user name' });
+        // Status 400:  bad request/client error
+        res.json({ message: 'wrong user name', status: 400 });
       }
     })
 });
 
 
 
-// POST LOGOUT (user logs out using JWT token that expires immediately) (Accessible by All Users)
+
+// POST LOGOUT (user logs out using JWT token that expires immediately). (Accessible by All Users)
 router.post('/logout', function (req, res) {
   res.cookie('jwt', '', { expires: new Date(0) });
   res.json({ message: 'Successfully logged out' });
@@ -70,7 +78,8 @@ router.post('/logout', function (req, res) {
 
 
 
-// GET ALL users listing.  (Admin Protected)
+
+// GET ALL users listing.  This route will use the authorization service .verifyUser() method.  (Admin Protected)
 router.get('/', function (req, res) {
   let token = req.cookies.jwt;
   if (token) {
@@ -101,17 +110,44 @@ router.get('/', function (req, res) {
             });
           })
         } else {
+          // Status 401: unauthorized client/lacks valid authentication credentials 
           res.status(401).json({ message: 'Unauthorized User' });
         }
       });
   } else {
+    // Status 401: unauthorized client/lacks valid authentication credentials 
     res.status(401).json({ message: 'Must be logged in' });
   };
 });
 
 
 
-// GET BY ID users listing  (Admin Protected)
+// This route will use the authentication service .verifyUser() method to determine if a user can be found based on the JWT token stored as a cookie. 
+router.get('/profile', function (req, res) {
+  // let token = req.cookies.jwt;
+  let token = req.headers['authorization'];
+  console.log(token);
+  token = token.slice(4)
+  if (token) {
+    authService.verifyUser(token)
+      .then(user => {
+        if (user) {
+          // Status 200: OK status/success
+          res.json({ user, status: 200 })
+        } else {
+          // Status 403: Forbidden / server understood the request, but refused to authorize
+          res.json({ message: 'Invalid authentication token', status: 403 });
+        }
+      });
+  } else {
+    // Status 403: Forbidden / server understood the request, but refused to authorize
+    res.json({ message: 'Must be logged in', status: 403 });
+  };
+});
+
+
+
+// GET BY ID users listing.  This route will use the authorization service .verifyUser() method.  (Admin Protected)
 router.get('/:id', function (req, res) {
   let token = req.cookies.jwt;
   if (token) {
@@ -148,18 +184,19 @@ router.get('/:id', function (req, res) {
               }
             })
         } else {
+          // Status 401: unauthorized client/lacks valid authentication credentials 
           res.status(401).json({ message: 'Unauthorized User' });
         }
       });
   } else {
+    // Status 401: unauthorized client/lacks valid authentication credentials 
     res.status(401).json({ message: 'Must be logged in' });
   };
 });
 
 
 
-
-// PUT Update User by ID  (Admin Protected)
+// PUT Update User by ID.  This route will use the authorization service .verifyUser()method.  (Admin Protected)
 router.put("/:id", function (req, res) {
   let token = req.cookies.jwt;
   if (token) {
@@ -171,20 +208,23 @@ router.put("/:id", function (req, res) {
             .update(req.body, { where: { UserId: userId } })
             .then(result => res.redirect('/users/' + userId))
             .catch(err => {
+              // Status 400:  bad request/client error
               res.status(400).json({ message: "There was a problem updating the user. Please check the user information." });
             });
         } else {
+          // Status 401: unauthorized client/lacks valid authentication credentials 
           res.status(401).json({ message: 'Unauthorized User' });
         }
       });
   } else {
+    // Status 401: unauthorized client/lacks valid authentication credentials 
     res.status(401).json({ message: 'Must be logged in' });
   };
 });
 
 
 
-// DELETE User by ID (Admin Protected)
+// DELETE User by ID. This route will use the authorization service .verifyUser()method. (Admin Protected)
 router.delete("/:id", function (req, res) {
   let token = req.cookies.jwt;
   if (token) {
@@ -196,39 +236,18 @@ router.delete("/:id", function (req, res) {
             .update({ Deleted: true }, { where: { UserId: UserId } })
             .then(result => res.json({ message: 'User successfully deleted.' }))
             .catch(err => {
+              // Status 400:  bad request/client error
               res.status(400).json({ message: "There was a problem deleting the product. Please make sure you are specifying the product id." });
             })
         } else {
+          // Status 401: unauthorized client/lacks valid authentication credentials 
           res.status(401).json({ message: 'Unauthorized User' });
         }
       })
   } else {
+    // Status 401: unauthorized client/lacks valid authentication credentials 
     res.status(401).json({ message: 'Must be logged in' });
   }
 });
 
-
 module.exports = router;
-
-
-
-
-
-// **************************Router.GET PROFILE ROUTE****************************
-// GET PROFILE (a user will only be able to access and view their profile and no one else's profile once they are logged in). This route will use the authentication service to determine if a user can be found based on the JWT token stored as a cookie. This route will use the authentication service .verifyUser() method.
-// router.get('/profile', function(req, res) {
-//   let token = req.cookies.jwt;
-//   if (token) {
-//   authService.verifyUser(token)
-//     .then(user => {
-//       if (user) {
-//         res.json({ user} )
-//       } else {
-//         res.status(401).json({ message: 'Invalid authentication token'});
-//       }
-//     });
-//   } else {
-//     res.status(401).json({ message: 'Must be logged in'});
-//   };
-// });
-// **************************Router.GET PROFILE ROUTE********************************
